1aada026f46f04da4b124133515479df5f8af46b
beiconmb-glitch <beiconmb@gmail.com>
2026-07-26 14:27:31 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,6 +1,6 @@
 -- JUJU HUB - Panel de Login para Roblox
 local KEY_CORRETA = "JUJU-HGSCRIPT23"
-local SCRIPT_URL = "raw.githubusercontent.com/beiconmb-glitch/JUJU-HUB/refs/heads/main/Script.lua https://share.google/L4rTwGITbcOlYH2Np"
+local SCRIPT_URL = "https://raw.githubusercontent.com/timmyVeryCrazy/FleeTheFacilityCheat/refs/heads/main/FleeTheFacilityCheatScript"
 
 -- Criando a Interface (ScreenGui)
 local ScreenGui = Instance.new("ScreenGui")
